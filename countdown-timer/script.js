52de2b7a974cfd3c843099fda7a85a25a75c3284
Sreekant Baheti <60787859+Sreekant13@users.noreply.github.com>
2020-09-30 00:18:06 +0530

    Update script.js

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById('hours');
 const minsEl = document.getElementById('mins');
 const secondsEl = document.getElementById('seconds');
 
-const newYears = '19 Oct 2020';
+const newYears = '1 Jan 2021';
 function countdown(){
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
